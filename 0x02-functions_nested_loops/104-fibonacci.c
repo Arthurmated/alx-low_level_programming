0ec87a693d682d9a97dc5581bbdb3bd7159b7876
Arthurmated <tajmated18@gmail.com>
2022-03-20 00:04:06 -0700

    editing fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,8 +3,8 @@
 /**
  *  * main - main block
  *   * Description: Find and print the first 98 fib numbers starting with 1 and 2.
- *    * Numbers should be coma and space separated.
- *     * Return: 0
+ * Numbers should be coma and space separated.
+ * Return: 0
 */
 int main(void)
 {
